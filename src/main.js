c9ce4eb69971d1ddc8c610e236fb611af22ef3d0
flowering-calathea <287562121+flowering-calathea@users.noreply.github.com>
2026-05-24 22:53:43 -0400

    Some files yay

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const coordsValue = document.getElementById('coords-value');
+const btnGeo      = document.getElementById('btn-geo');
+const btnPoster   = document.getElementById('btn-geo');
+
+btnGeo.addEventListener('click', async () => {
+  document.body.style.backgroundColor = 'var(--cool)';
+});
